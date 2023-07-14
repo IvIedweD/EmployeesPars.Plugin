9f8c5530b0f4648fdc379fe21bd7d54ecdba5e57
IvIedweD <82469975+IvIedweD@users.noreply.github.com>
2023-07-14 17:13:28 +0600

    Add files via upload

---
+++ EmployeesPars.Plugin/Plugin.cs
@@ -0,0 +1,35 @@
+﻿using Newtonsoft.Json;
+using PhoneApp.Domain.Attributes;
+using PhoneApp.Domain.DTO;
+using PhoneApp.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+
+namespace EmployeesPars.Plugin
+{
+    [Author(Name = "Mikhail Pestryakov")]
+
+    public class Pars : IPluggable
+    {
+        public IEnumerable<DataTransferObject> Run(IEnumerable<DataTransferObject> args)
+        {
+            string fileName = "users.json";
+            string jsonString = File.ReadAllText(fileName);            
+            var employeesList = args.Cast<EmployeesDTO>().ToList();
+            var peoples = JsonConvert.DeserializeObject<Rootobject>(jsonString);
+            foreach(var people in peoples.users)
+            {
+                employeesList.Add(new EmployeesDTO() { Name = people.lastName + " " + people.firstName, Phone = people.phone });
+            }
+            return employeesList;
+
+
+        }
+    }
+}
---
+++ EmployeesPars.Plugin/Rootobject.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeesPars.Plugin
+{
+
+    public class Rootobject
+    {
+        public User[] users { get; set; }
+    }
+}
